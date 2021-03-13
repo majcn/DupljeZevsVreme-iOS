fa15fef4da7544b2c8dba1654a9b17d3feedb350
Gregor Majcen <gregor.majcen@sinergise.com>
2021-03-13 21:02:53 +0100

    date formatters

--- Shared/Util/DateExtension.swift
+++ Shared/Util/DateExtension.swift
@@ -14,8 +14,17 @@ extension Date {
     return formatter
   }()
 
+  private static let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    return formatter
+  }()
 
   func timeToString() -> String {
     return Date.timeFormatter.string(from: self)
+  }
+
+  func dateToString() -> String {
+    return Date.dateFormatter.string(from: self)
   }
 }
