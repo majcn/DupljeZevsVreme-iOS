7602af470f96945bbda648a3bcc1d3efb258a7ed
Gregor Majcen <gregor.majcen@sinergise.com>
2021-03-13 21:02:53 +0100

    main view

--- DupljeZevsVreme/View/ContentView.swift
+++ DupljeZevsVreme/View/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct ContentView: View {
+  @ObservedObject var dupljeZevsVremeViewModel = DupljeZevsVremeViewModel()
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      List {
+        WeatherListView(model: dupljeZevsVremeViewModel.temperatureViewModel)
+        WeatherListView(model: dupljeZevsVremeViewModel.humidityViewModel)
+        WeatherListView(model: dupljeZevsVremeViewModel.windViewModel)
+        WeatherListView(model: dupljeZevsVremeViewModel.rainViewModel)
+        WeatherListView(model: dupljeZevsVremeViewModel.sunViewModel)
+        WeatherListView(model: dupljeZevsVremeViewModel.pressureViewModel)
+      }
+      .listStyle(GroupedListStyle())
+      .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+        if dupljeZevsVremeViewModel.fetch() {
+          WidgetCenter.shared.reloadAllTimelines()
+        }
+      }
+      .onAppear {
+        if dupljeZevsVremeViewModel.fetch() {
+          WidgetCenter.shared.reloadAllTimelines()
+        }
+      }
+      .navigationTitle("Vreme")
+      .toolbar {
+        ToolbarItem(placement: .bottomBar) {
+          Text("Posodobljeno \(dupljeZevsVremeViewModel.updated.dateToString()) ob \(dupljeZevsVremeViewModel.updated.timeToString())")
+        }
+      }
+    }
+    .navigationViewStyle(StackNavigationViewStyle())
   }
 }
 
