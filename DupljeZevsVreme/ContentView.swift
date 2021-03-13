20c8f61f61bc496c814685f3677d24478f37ec3c
Gregor Majcen <gregor.majcen@sinergise.com>
2021-03-13 21:02:53 +0100

    Initial Commit

---
+++ DupljeZevsVreme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DupljeZevsVreme
+//
+//  Created by Gregor Majcen on 12/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DupljeZevsVreme/DupljeZevsVremeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DupljeZevsVremeApp.swift
+//  DupljeZevsVreme
+//
+//  Created by Gregor Majcen on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DupljeZevsVremeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
